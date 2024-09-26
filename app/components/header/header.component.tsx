c7c9c0d176da8484c3cc69093d8e67ceaef62501
Mohammad-Hossein-Ostad <rajabaliostad@gmail.com>
2024-09-26 20:49:31 +0200

    fix: fixed some typo

--- app/components/header/header.component.tsx
+++ app/components/header/header.component.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           href="/"
-          className="flex items-start space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             SL TRAFIK INFO
@@ -21,7 +21,7 @@ export default function Header() {
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
-                TunnelBana
+                Tunnelbana
               </Link>
             </li>
             <li>
@@ -45,7 +45,7 @@ export default function Header() {
                 href="/tvarbanan"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Tvärbannan
+                Tvärbana
               </Link>
             </li>
           </ul>
