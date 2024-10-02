bcc5d79a1ff739e7fae1aa116d3b2095a29001fa
Mohammad-Hossein-Ostad <rajabaliostad@gmail.com>
2024-10-02 12:05:12 +0200

    minor update for improve/handling the fetching data

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,3 @@
-export const dynamicParams = false;
-
 type MessageVariants = {
   header: string;
   details: string;
@@ -15,15 +13,23 @@ type MessageResponse = {
   scope: Scope;
 }[];
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   const posts: MessageResponse = await fetch(
     "https://deviations.integration.sl.se/v1/messages",
     { cache: "no-store" }
   ).then((res) => res.json());
 
-  return posts.map((post) => ({
-    slug: post.scope.lines[0].transport_mode.toLowerCase(),
-  }));
+  return posts.map((post) => {
+    const lines = post.scope.lines;
+    if (lines && lines.length > 0) {
+      const { transport_mode } = lines[0];
+      return { slug: transport_mode.toLowerCase() };
+    }
+
+    return { slug: "unknown" };
+  });
 }
 
 // Page component handleing dynamic slugs
@@ -41,10 +47,11 @@ export default async function Page({ params }: { params: { slug: string } }) {
           const { deviation_case_id, message_variants, scope } = message;
           const { lines } = scope;
 
-          if (lines && lines.length > 0) {
+          if (lines && lines.length > 0 && message_variants.length > 0) {
             const { transport_mode } = lines[0];
             const { header, details } = message_variants[0];
-            if (transport_mode.toLowerCase() === slug) {
+
+            if (transport_mode?.toLowerCase() === slug) {
               return (
                 <div key={deviation_case_id}>
                   <h3 className="text-2xl">{header}</h3>
